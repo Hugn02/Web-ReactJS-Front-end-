d54561b908d6fe2c630f2a088f3a7d2e3a1c4446
Hugn02 <29112002.hn@gmail.com>
2024-11-12 18:28:14 +0700

    Mô tả thay đổi

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import './Footer.css'
 import footer_logo from '../Assets/logo_big.png'
+import fb_icon from '../Assets/facebook_icon.png'
 import instagram_icon from '../Assets/instagram_icon.png'
-import pintester_icon from '../Assets/pintester_icon.png'
-import whatsapp_icon from '../Assets/whatsapp_icon.png'
+import tiktok_icon from '../Assets/tiktok_icon.png'
 
 const Footer = () => {
     return (
@@ -21,13 +21,34 @@ const Footer = () => {
             </ul>
             <div className="footer-social-icon">
                 <div className="footer-icons-container">
-                    <img src={instagram_icon} alt="" />
+                <a
+        href="https://www.facebook.com/profile.php?id=100011586374194" // Thay bằng URL Facebook của bạn
+        target="_blank" // Mở liên kết trong tab mới
+        rel="noopener noreferrer" // Đảm bảo an toàn cho liên kết
+        aria-label="Facebook"
+      >
+                    <img src={fb_icon} alt="Facebook" />
+                    </a>
                 </div>
                 <div className="footer-icons-container">
-                    <img src={pintester_icon} alt="" />
+                <a
+        href="https://www.instagram.com/duongvanhung2002.hn/" // Thay bằng URL Facebook của bạn
+        target="_blank" // Mở liên kết trong tab mới
+        rel="noopener noreferrer" // Đảm bảo an toàn cho liên kết
+        aria-label="Instagram"
+      >
+                    <img src={instagram_icon} alt="Instagram" />
+                    </a>
                 </div>
                 <div className="footer-icons-container">
-                    <img src={whatsapp_icon} alt="" />
+                <a
+        href="https://www.tiktok.com/@user4157124300746" // Thay bằng URL Facebook của bạn
+        target="_blank" // Mở liên kết trong tab mới
+        rel="noopener noreferrer" // Đảm bảo an toàn cho liên kết
+        aria-label="TikTok"
+      >
+                    <img src={tiktok_icon} alt="TikTok" />
+                    </a>
                 </div>
             </div>
             <div className="footer-copyright">
